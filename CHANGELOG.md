02c6171c49de3cdf45ee730f56e76906ded5eb1d
Alberto Morón Hernández <alberto.moronh@gmail.com>
2024-04-19 08:45:02 +0100

    feat: add skeletal source tree to build upon

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,3 +6,10 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+
+### Added
+
+- eleventy as the core dependency to build the static site.
+- A skeletal `src/` directory to build on.
+- Build and local dev configuration.
+- A justfile to run project commands (eg. build, serve).
---
+++ src/_data/metadata.js
@@ -0,0 +1,22 @@
+module.exports = function() {
+    const staticOrigin = "https://pub-ea1b3388186e482ea39aa713d3e5aa47.r2.dev/static/";
+    const speculumRelease = "speculum@1.3.0";
+
+    const today8601 = new Date().toISOString().slice(0, 10);
+
+    let footerDate = "2024";
+    const curYear = parseInt(today8601.slice(0, 4));
+    if (curYear > parseInt(footerDate)) {
+        footerDate = `${footerDate}-${curYear}`;
+    }
+
+    return {
+        title: "DepositDuck",
+        domain: "example.com",  // TODO: read in from environment variables
+        origin: "https://example.com",  // TODO: read in from environment variables
+        speculumSource: `${staticOrigin}${speculumRelease}`,
+        createdAt: today8601,
+        updatedAt: today8601,
+        footerDate
+    };
+};
